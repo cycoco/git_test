5422c3f431ede43698f2e71e6cfd1005d5472730
cy <zy@163.com>
2021-08-27 18:50:45 +0800

    git_test_fori

--- src/main/java/com/cy/Git_Test.java
+++ src/main/java/com/cy/Git_Test.java
@@ -2,6 +2,8 @@ package com.cy;
 
 public class Git_Test {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
